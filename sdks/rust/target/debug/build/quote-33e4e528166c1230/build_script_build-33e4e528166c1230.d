524c47281b0e005ea5b9eccd67cbf9ad64028c41
Donald Murillo <54650861+DonaldMurillo@users.noreply.github.com>
2026-04-30 15:42:36 -0400

    chore: remove SDK build artifacts from git, add .gitignore

--- sdks/rust/target/debug/build/quote-33e4e528166c1230/build_script_build-33e4e528166c1230.d
+++
@@ -1,5 +0,0 @@
-/Users/dom/programming/statico/sdks/rust/target/debug/build/quote-33e4e528166c1230/build_script_build-33e4e528166c1230.d: /Users/dom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/dom/programming/statico/sdks/rust/target/debug/build/quote-33e4e528166c1230/build_script_build-33e4e528166c1230: /Users/dom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/Users/dom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
